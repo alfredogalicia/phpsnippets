bcea95a22e0f96191dfd6485dd6e23fbfbb6295c
Fredd Galicia <66749311+alfredogalicia@users.noreply.github.com>
2022-09-22 12:24:32 -0500

    Update sendmail.php
    Additional changes

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,11 @@
-$from    = 'fredd@yourwebsite.com';
-$to      = 'nobody@example.com';
-$subject = 'Your Subject';
-// Plain message
-$message = 'Hello! How are you today?';
-// HTML message
-$message = '<p>Hello! How are you today?</p>';
-$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'Return-Path: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
-mail($to, $subject, $message, $headers);
+$to = "subject@mail.com";  //This is where you place the receptors mail
+$subject = "Mail testing"; //Title of your mail
+$body = "Body of your message here you can use HTML too. e.g. <br> <b> Bold </b>";
+$headers = "From: Fredd\r\n";
+$headers .= "Reply-To: info@yoursite.com\r\n";
+$headers .= "Return-Path: info@yoursite.com\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+mail($to,$subject,$body,$headers);
+?> 
